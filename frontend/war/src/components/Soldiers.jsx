536c034fcc935b261462295fb5106193bd0d91f5
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-14 16:36:18 +0300

    add picture

--- frontend/war/src/components/Soldiers.jsx
+++ frontend/war/src/components/Soldiers.jsx
@@ -191,8 +191,8 @@ const Soldiers = () => {
                     </button>
                 </div>
                 {loading ? (
-                    <div className="flex justify-center items-center">
-                        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+                    <div className='bg-white'>
+                        <p className=''>Loading...</p>
                     </div>
                 ) : (
                     <>
