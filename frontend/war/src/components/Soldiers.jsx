b249f41d23bfa69b6bd5ba0e0ea988a0f5b3c8ab
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-14 15:11:03 +0300

    soldiers

--- frontend/war/src/components/Soldiers.jsx
+++ frontend/war/src/components/Soldiers.jsx
@@ -4,13 +4,11 @@ import { useNavigate } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchSoliders, setSoliders } from '../features/soliderSlice';
 
-
 const Soldiers = () => {
-    const nav = useNavigate()
+    const nav = useNavigate();
     const soldiers = useSelector(state => state.solider.soliders);
     const searchSoldiers = useSelector(state => state.solider.searchSoliders);
     const solidersArr = searchSoldiers.length > 0 ? searchSoldiers : soldiers;
-    // const [soldiers, setSoldiers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [count, setCount] = useState(1);
     const [isNext, setIsNext] = useState(false);
@@ -20,107 +18,92 @@ const Soldiers = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [loading, setLoading] = useState(false);
     const user = useSelector(state => state.user.connectedUser);
-    // const pageSize = 30;
-    // const totalCount = 0;
     const dispatch = useDispatch();
 
     const fetchSoldiers = async (page) => {
+        setLoading(true);
         try {
             const data = await getSoldiers(page);
-            console.log('API Response:', data);
-            // setSoldiers(data);
             dispatch(setSoliders(data));
-            if (page == 1) {
+            if (page === 1) {
                 setIsPrev(false);
                 setIsNext(true);
-            }
-            else if (page < count) {
+            } else if (page < count) {
                 setIsNext(true);
                 setIsPrev(true);
-            }
-            else if (page == count) {
+            } else if (page === count) {
                 setIsNext(false);
             }
         } catch (error) {
-            // setCurrentPage(page);
-            // Handle error
+            console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
 
     useEffect(() => {
-        if (count == 1 && !isNext && !isPrev) {
+        if (count === 1 && !isNext && !isPrev) {
             GetCountSoliders().then(res => {
-                console.log("count", count)
                 setCount(res);
-            })
+            });
         }
-        if (isNext && searchQuery || isPrev && searchQuery) {
+        if ((isNext && searchQuery) || (isPrev && searchQuery)) {
             globalSearchSoldiers(searchQuery, currentPage).then(res => {
                 if (res.length > 30) {
                     setIsNext(true);
                     let a = res;
                     a.splice(res.length - 1, 1);
                     dispatch(setSearchSoliders(a));
-                }
-                else {
+                } else {
                     setIsNext(false);
                     dispatch(setSearchSoliders(res));
                 }
             });
             if (currentPage > 1) {
                 setIsPrev(true);
-            }
-            else {
+            } else {
                 setIsPrev(false);
             }
-        }
-        else {
+        } else {
             fetchSoldiers(currentPage);
         }
-
     }, [currentPage]);
 
     const handlePageChange = (newPage) => {
-
-        console.log("ffffffff", newPage);
         setCurrentPage(newPage);
     };
-
 
     const handleSearchValue = (e) => {
         let searchValue = e.target.value;
         setSearchQuery(searchValue);
         setSearchMessage("");
-        if (searchValue != "") {
+        if (searchValue !== "") {
             globalSearchSoldiers(searchValue, currentPage).then(res => {
                 if (res.length > 30) {
                     setIsNext(true);
                     let a = res;
                     a.splice(res.length - 1, 1);
                     dispatch(setSearchSoliders(a));
-                }
-                else {
+                } else {
                     setIsNext(false);
                     dispatch(setSearchSoliders(res));
-                    if (res.length == 0) {
+                    if (res.length === 0) {
                         setSearchMessage("no result :(");
                     }
                 }
             });
             if (currentPage > 1) {
                 setIsPrev(true);
-            }
-            else {
+            } else {
                 setIsPrev(false);
             }
-        }
-        else {
+        } else {
             setIsNext(false);
             setIsPrev(false);
             setCurrentPage(1);
             dispatch(setSearchSoliders([]));
         }
-    }
+    };
 
     return (
         <div className="bg-gray-200">
@@ -130,7 +113,7 @@ const Soldiers = () => {
                 </svg>
             </button>
             {isOpen && (
-                <nav className="lg:hidden md:hidden sm:hidden left-0 top-0 flex shadow bg-white  justify-around items-center text-black lg:text-3xl lg:h-[80px] md:text-2m md:h-[30px] sm:text-sm  text-xs mt-4 sm:mt-0 font-normal font-['Alef'] leading-[45px] cursor-pointer">
+                <nav className="lg:hidden md:hidden sm:hidden left-0 top-0 flex shadow bg-white justify-around items-center text-black lg:text-3xl lg:h-[80px] md:text-2m md:h-[30px] sm:text-sm text-xs mt-4 sm:mt-0 font-normal font-['Alef'] leading-[45px] cursor-pointer">
                     {!user && (
                         <>
                             <div onClick={() => nav('/register')}>הרשמה</div>
@@ -149,12 +132,12 @@ const Soldiers = () => {
                     <div onClick={() => nav('/soldiers')} className='font-bold'>
                         חיפוש
                     </div>
-                    <div onClick={() => nav('/homePage')} >
+                    <div onClick={() => nav('/homePage')}>
                         אודות
                     </div>
                 </nav>
             )}
-            <nav className="hidden lg:flex md:flex sm:flex left-0 top-0 shadow bg-white  justify-center  items-center  text-black lg:text-2xl  lg:h-[47px] md:text-xl md:h-[40px] sm:text-s  sm:h-[20px] mt-4 sm:mt-0 font-normal font-['Alef'] leading-[45px] cursor-pointer space-x-11">
+            <nav className="hidden lg:flex md:flex sm:flex left-0 top-0 shadow bg-white justify-center items-center text-black lg:text-2xl lg:h-[47px] md:text-xl md:h-[40px] sm:text-s sm:h-[20px] mt-4 sm:mt-0 font-normal font-['Alef'] leading-[45px] cursor-pointer space-x-11">
                 {!user && (
                     <>
                         <div onClick={() => nav('/register')}>הרשמה</div>
@@ -178,15 +161,16 @@ const Soldiers = () => {
             </nav>
             <div className="bg-gray-200">
                 <div className=''>
-                    <h2 className=" text-black text-4xl font-bold font-['Alef'] mb-6 mt-6 text-center">לזכרם של הנופלים במלחמת חרבות ברזל</h2>
+                    <h2 className="text-black text-4xl font-bold font-['Alef'] mb-6 mt-6 text-center">לזכרם של הנופלים במלחמת חרבות ברזל</h2>
                 </div>
                 <div className="text-center mb-4">
                     <input
-                        type=""
+                        type="text"
                         placeholder="חיפוש"
                         value={searchQuery}
                         onChange={handleSearchValue}
-                        className="border border-black px-4 py-2 rounded-md" style={{ direction: 'rtl' }}
+                        className="border border-black px-4 py-2 rounded-md"
+                        style={{ direction: 'rtl' }}
                     />
                 </div>
                 <div className="flex justify-center items-center mt-8">
@@ -200,7 +184,7 @@ const Soldiers = () => {
                     <span className="text-lg font-bold"> {currentPage}</span>
                     <button
                         onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage == count && !isNext || !isNext}
+                        disabled={currentPage === count && !isNext || !isNext}
                         className="px-4 py-1 bg-gray-200 text-black rounded-md ml-4 border border-black"
                     >
                         דף הבא
@@ -211,42 +195,43 @@ const Soldiers = () => {
                         <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
                     </div>
                 ) : (
-                    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2">
-                        {searchMessage === "" ? solidersArr.map((soldier) => (
-                            <div key={soldier.Id} className="text-center  border border-gray-500 rounded-sm p-2">
-                                <div className='flex justify-center'>
-                                    <img className="h-64 w-64 object-cover" src={soldier.Image} alt={`${soldier.firstName} ${soldier.lastName}`} />
+                    <>
+                        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2">
+                            {searchMessage === "" ? solidersArr.map((soldier) => (
+                                <div key={soldier.Id} className="text-center border border-gray-500 rounded-sm p-2">
+                                    <div className='flex justify-center'>
+                                        <img className="h-64 w-64 object-cover" src={soldier.Image} alt={`${soldier.FirstName} ${soldier.LastName}`} />
+                                    </div>
+                                    <h3>{`${soldier.FirstName} ${soldier.LastName}`}</h3>
+                                    <p>{`גיל: ${soldier.Age}`}</p>
+                                    <p>{`עיר: ${soldier.City}`}</p>
+                                    <button className="font-semibold mb-2 mt-4 border border-black text-black p-2 rounded-md hover:opacity-80 hover:text-black hover:font-extrabold hover:border-black" onClick={() => nav(`/soldierInfo/${soldier.Id}`)}>לפרטים נוספים</button>
                                 </div>
-                                <h3>{`${soldier.FirstName} ${soldier.LastName}`}</h3>
-                                <p>{`גיל: ${soldier.Age}`}</p>
-                                <p>{`עיר: ${soldier.City}`}</p>
-                                <button className="font-semibold mb-2 mt-4 border border-black text-black p-2 rounded-md hover:opacity-80  hover:text-black hover:font-extrabold hover:border-black" onClick={() => nav(`/soldierInfo/${soldier.Id}`)}>לפרטים נוספים</button>
-                            </div>
-                        )) : <span>{searchMessage}</span>}
-                    </div>
+                            )) : <span>{searchMessage}</span>}
+                        </div>
+                        <div className="flex justify-center items-center mt-8">
+                            <button
+                                onClick={() => handlePageChange(currentPage - 1)}
+                                disabled={currentPage === 1 && !isPrev}
+                                className="px-4 py-1 bg-gray-200 text-black rounded-md mr-4 border border-black"
+                            >
+                                דף קודם
+                            </button>
+                            <span className="text-lg font-bold"> {currentPage}</span>
+                            <button
+                                onClick={() => handlePageChange(currentPage + 1)}
+                                disabled={currentPage === count && !isNext || !isNext}
+                                className="px-4 py-1 bg-gray-200 text-black rounded-md ml-4 border border-black"
+                            >
+                                דף הבא
+                            </button>
+                        </div>
+                    </>
+
                 )}
-
-                <div className="flex justify-center items-center mt-8">
-                    <button
-                        onClick={() => handlePageChange(currentPage - 1)}
-                        disabled={currentPage === 1 && !isPrev}
-                        className="px-4 py-1 bg-gray-200 text-black rounded-md mr-4 border border-black"
-                    >
-                        דף קודם
-                    </button>
-                    <span className="text-lg font-bold"> {currentPage}</span>
-                    <button
-                        onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage == count && !isNext || !isNext}
-                        className="px-4 py-1 bg-gray-200 text-black rounded-md ml-4 border border-black"
-                    >
-                        דף הבא
-                    </button>
-                </div>
-            </div >
-
-            );
+            </div>
+        </div>
+    );
 };
 
-            export default Soldiers;
-
+export default Soldiers;
