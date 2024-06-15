0f9ec7b1ec183aacc0e92886d33a1d2d0232a0df
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-15 23:48:28 +0300

    add picture

--- frontend/war/src/components/AddPicture.jsx
+++ frontend/war/src/components/AddPicture.jsx
@@ -6,7 +6,7 @@ import { storage } from "./firebase";
 import { v4 as uuidv4 } from "uuid";
 import { addPicture } from '../utils/PictureUtil';
 import emailjs from 'emailjs-com';
-import Sidebar from './Sidebar';
+import { FaHome, FaUserAlt, FaGraduationCap, FaLayerGroup, FaStickyNote, FaComments, FaBars } from 'react-icons/fa';
 
 const AddPicture = () => {
     const nav = useNavigate();
@@ -118,8 +118,44 @@ const AddPicture = () => {
 
     return (
         <div className="bg-gray-200 h-screen">
+            <button className="lg:hidden md:hidden sm:hidden" onClick={() => { setIsOpen(!isOpen) }} >
+                <FaBars className="text-white" />
+            </button>
+            {isOpen && (
+                <nav className="lg:hidden md:hidden sm:hidden left-0 top-0 flex shadow bg-white justify-around items-center text-black lg:text-3xl lg:h-[80px] md:text-2m md:h-[30px] sm:text-sm text-xs mt-4 sm:mt-0 font-normal font-['Alef'] leading-[45px] cursor-pointer">
+                    {!user && (
+                        <>
+                            <div onClick={() => nav('/register')}> <FaUserAlt /></div>
+                            <div onClick={() => nav('/')}> <FaUserAlt /></div>
+                        </>
+                    )}
+                    {user && (
+                        <>
+                            <div onClick={() => nav('/logOut')}> <FaUserAlt /></div>
+                        </>
+                    )}
+                    <div onClick={() => nav('/contact')}> <FaUserAlt /></div>
+                    <div onClick={() => nav('/soldiers')} className='font-bold'> <FaUserAlt /></div>
+                    <div onClick={() => nav('/homePage')}> <FaUserAlt /></div>
+                </nav>
+            )}
+            <nav className="hidden lg:flex md:flex sm:flex left-0 top-0 shadow bg-white justify-center items-center text-black lg:text-2xl lg:h-[47px] md:text-xl md:h-[40px] sm:text-s sm:h-[20px] mt-4 sm:mt-0 font-normal font-['Alef'] leading-[45px] cursor-pointer space-x-11">
+                {!user && (
+                    <>
+                        <div onClick={() => nav('/register')}>הרשמה</div>
+                        <div onClick={() => nav('/')}>התחברות</div>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <div onClick={() => nav('/logOut')}>התנתקות</div>
+                    </>
+                )}
+                <div onClick={() => nav('/contact')}>צור קשר</div>
+                <div onClick={() => nav('/soldiers')} className='font-bold'>חיפוש</div>
+                <div onClick={() => nav('/homePage')}>אודות</div>
+            </nav>
 
-            <Sidebar />
             <div className='flex items-center mb-1'>
                 <img className="mt-3 ml-5 max-w-[1%] lg:max-w-[1%] lg:mr-15 md:max-w-[1%] sm:max-w-[1%]" src="/חץ חזור.svg" alt="Logo" onClick={() => nav(-1)} />
             </div>
