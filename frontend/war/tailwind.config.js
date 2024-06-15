9f55aeae3b4b76bcbb9e8cb12e6f055b46df8bfe
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-15 22:35:58 +0300

    picture

--- frontend/war/tailwind.config.js
+++ frontend/war/tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Open Sans', 'sans-serif'],
       },
+      // Add custom utilities to extend
+      utilities: {
+        '.writing-mode-vertical-rl': {
+          writingMode: 'vertical-rl',
+        },
+        '.rotate-180': {
+          transform: 'rotate(180deg)',
+        },
+      }
     },
   },
   plugins: [],
 }
-
