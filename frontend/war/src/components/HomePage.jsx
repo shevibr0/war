39f8917a44a6419bf2856ddd3519fcaafc187287
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-16 22:56:59 +0300

    loading

--- frontend/war/src/components/HomePage.jsx
+++ frontend/war/src/components/HomePage.jsx
@@ -22,8 +22,8 @@ const HomePage = () => {
 
                 <div className="flex items-center justify-center  flex-wrap">
                     <div className="max-w-lg w-full mx-4  mt-4 p-3 text-center text-gray-600 text-xl font-light tracking-wider bg-white shadow-lg shadow-gray-800 rounded-2xl">
-                        המיזם "מנציחים את זיכרם" נוצר מתוך רצון להכיר את גיבורינו מקרוב ולהשאיר פיסות זיכרון נגישות לכולם<br />
-                        אנו מזמינים אתכם להוסיף מידע על חללים שהכרתם, כמו מתכון אהוב, תמונה משותפת או זיכרון מתוק שתרצו לשתף על יקירכם<br /><br />
+                        המיזם "מנציחים את זיכרם" נוצר מתוך רצון להכיר את גיבורינו מקרוב ולהשאיר פיסות זיכרון נגישות לכולם<br /><br />
+                        אנו מזמינים אתכם להוסיף מידע על חללים שהכרתם, כמו מתכון אהוב, תמונה משותפת או זיכרון מתוק שתרצו לשתף על יקירכם<br />
                         בנוסף, ישנה אפשרות לקחת התנדבות ולקרוא פרקי תהילים לעילוי נשמת החללים<br />
                     </div>
                 </div>
@@ -33,15 +33,15 @@ const HomePage = () => {
                     חיפוש</button>
             </div>
 
-            <div className="w-full bottom-0 mt-7 left-0  flex justify-center bg-gray-200">
+            <div className="w-full bottom-0 mt-7 left-0  flex justify-center bg-gray-200 text-gray-800">
                 <div className="flex items-center gap-4">
-                    <div className="relative transition duration-100 hover:text-yellow-400">
+                    <div className="relative transition duration-100 hover:text-yellow-400  text-xl">
                         <a href="https://www.linkedin.com/in/elisheva-einhoren-43b1b3220/" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
                     </div>
 
-                    <div className="relative transition duration-100 hover:text-yellow-400">
+                    <div className="relative transition duration-100 hover:text-yellow-400  text-xl">
                         <a href="mailto:mepe.leos@gmail.com">
                             <FontAwesomeIcon icon={faEnvelope} />
                         </a>
