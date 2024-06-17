fff22f3211592026b42738a94d09ea1fe2b11ec6
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-17 10:16:57 +0300

    add picture

--- frontend/war/src/components/SoldierInfo.jsx
+++ frontend/war/src/components/SoldierInfo.jsx
@@ -49,11 +49,11 @@ const SoldierInfo = () => {
         <div className="bg-gray-200 h-screen text-gray-800 px-4">
             <Sidebar />
             <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-2 items-center mt-0 font-bold'>
-                <div className='flex cursor-pointer bg-white shadow-lg shadow-gray-700 py-1 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/memories`)}>פתקי זכרון</div>
-                <div className='flex cursor-pointer border border-gray-600 p-1 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/pictures`)}>תמונות</div>
-                <div className='flex cursor-pointer border border-gray-600 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/theilim`)}>אמירת תהילים לעילוי נשמתו</div>
-                <div className='flex cursor-pointer border border-gray-600 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/volunteering`)}>התנדבויות לעילוי נשמתו</div>
-                <div className='flex cursor-pointer border border-gray-600 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/recepies`)}>מתכונים</div>
+                <div className='flex cursor-pointer bg-white shadow-lg shadow-gray-700 py-1 px-1 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/memories`)}>פתקי זכרון</div>
+                <div className='flex cursor-pointer  bg-white shadow-lg shadow-gray-700 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/pictures`)}>תמונות</div>
+                <div className='flex cursor-pointer  bg-white shadow-lg shadow-gray-700 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/theilim`)}>אמירת תהילים לעילוי נשמתו</div>
+                <div className='flex cursor-pointer  bg-white shadow-lg shadow-gray-700 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/volunteering`)}>התנדבויות לעילוי נשמתו</div>
+                <div className='flex cursor-pointer  bg-white shadow-lg shadow-gray-700 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/recepies`)}>מתכונים</div>
             </div>
             <div className='flex justify-center items-center bg-gray-200 mt-8'>
                 <div className="bg-white p-8 rounded-lg shadow-lg shadow-gray-500 w-full max-w-4xl">
