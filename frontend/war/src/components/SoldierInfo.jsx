dd33322d59b7a5133c971b98de152999ff090a4b
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-17 10:09:39 +0300

    picture

--- frontend/war/src/components/SoldierInfo.jsx
+++ frontend/war/src/components/SoldierInfo.jsx
@@ -46,17 +46,17 @@ const SoldierInfo = () => {
 
 
     return (
-        <div className=" bg-gray-200 h-screen text-gray-800">
+        <div className="bg-gray-200 h-screen text-gray-800 px-4">
             <Sidebar />
-            <div className='grid lg:grid-cols-5  md:grid-cols-3 sm:grid-cols-2 gap-2  items-center mt-0 font-bold'>
-                <div className='flex cursor-pointer bg-white shadow-lg shadow-gray-500  px-1 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/memories`)} >פתקי זכרון</div>
-                <div className='flex cursor-pointer border border-gray-600  p-1  rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/pictures`)} >תמונות</div>
-                <div className='flex cursor-pointer border border-gray-600  p-2  rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/theilim`)} >אמירת תהילים לעילוי נשמתו</div>
-                <div className='flex cursor-pointer border border-gray-600  p-2  rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/volunteering`)} >התנדבויות לעילוי נשמתו</div>
-                <div className='flex cursor-pointer border border-gray-600  p-2  rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/recepies`)} >מתכונים </div>
+            <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-2 items-center mt-0 font-bold'>
+                <div className='flex cursor-pointer bg-white shadow-lg shadow-gray-700 py-1 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/memories`)}>פתקי זכרון</div>
+                <div className='flex cursor-pointer border border-gray-600 p-1 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/pictures`)}>תמונות</div>
+                <div className='flex cursor-pointer border border-gray-600 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/theilim`)}>אמירת תהילים לעילוי נשמתו</div>
+                <div className='flex cursor-pointer border border-gray-600 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/volunteering`)}>התנדבויות לעילוי נשמתו</div>
+                <div className='flex cursor-pointer border border-gray-600 p-2 rounded-lg justify-center' onClick={() => nav(`/soldierInfo/${id}/recepies`)}>מתכונים</div>
             </div>
-            <div className='flex justify-center items-center bg-gray-200 shadow-lg shadow-gray-500'>
-                <div className="bg-white p-8 rounded-lg">
+            <div className='flex justify-center items-center bg-gray-200 mt-8'>
+                <div className="bg-white p-8 rounded-lg shadow-lg shadow-gray-500 w-full max-w-4xl">
                     {soldier !== null ? (
                         <div className="">
                             <h1 className='text-center text-2xl mb-2'>{`${soldier.FirstName} ${soldier.LastName}`}</h1>
@@ -70,28 +70,26 @@ const SoldierInfo = () => {
                                         <span>לחץ לשתף אנשים בקישור זה</span>
                                     </a>
                                 </div>
-                                <div >
+                                <div>
                                     {copySuccess && <p className="text-red-700">{copySuccess}</p>}
                                 </div>
-
                             </div>
-                            <div class="flex justify-center">
-                                <span class="bg-black w-56 h-[0.2px] mt-5"></span>
+                            <div className="flex justify-center">
+                                <span className="bg-black w-56 h-[0.2px] mt-5"></span>
                             </div>
                             <div className='text-center mt-4'>
                                 <p><strong>גיל:</strong> {soldier.Age}</p>
-                                {/* <p>{soldier.Gender}<strong> :מין</strong></p> */}
                                 <p><strong>עיר:</strong> {soldier.City}</p>
-                                <p>{new Date(soldier.DateOfDeath).toDateString()}<strong> :תאריך פטירה</strong> </p>
+                                <p><strong>תאריך פטירה לועזי:</strong> {new Date(soldier.DateOfDeath).toDateString()}</p>
+                                <p><strong>תאריך פטירה עברי:</strong> {hebrewDateString}</p>
                                 <p><strong>מקום הפטירה:</strong> {soldier.PlaceOfDeath}</p>
                                 <p><strong>דרגה:</strong> {soldier.RankName}</p>
                                 <p><strong>תפקיד:</strong> {soldier.Role}</p>
                                 <p><strong>תאור קצר:</strong> {soldier.ShortDescription}</p>
                                 <div className='flex justify-center'>
-                                    <p className='w-56'><strong >:תאור ארוך</strong> {soldier.LongDescription}</p>
+                                    <p className='w-56'><strong>:תאור ארוך</strong> {soldier.LongDescription}</p>
                                 </div>
-                                <p> <a target="_blank" href={soldier.UrlToArticle}>{soldier.UrlToArticle}<strong>:כתובת אתר למאמר</strong></a></p>
-                                {/* <p> {soldier.Classification} <strong>:סיווג</strong> </p> */}
+                                <p><a target="_blank" href={soldier.UrlToArticle}>{soldier.UrlToArticle}<strong>:כתובת אתר למאמר</strong></a></p>
                                 <p><strong>מקום השירות:</strong> {soldier.PlaceOfService}</p>
                                 <p><strong>מקום הקבורה:</strong> {soldier.BurialPlace}</p>
                                 <p><strong>ילד:</strong> {soldier.IsChild ? 'כן' : 'לא'}</p>
@@ -109,5 +107,4 @@ const SoldierInfo = () => {
         </div>
     );
 };
-
 export default SoldierInfo;
