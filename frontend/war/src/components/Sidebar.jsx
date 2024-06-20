4abf69c0781a07ced538a6b075dd378acce98bbc
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-20 23:56:56 +0300

    jj

--- frontend/war/src/components/Sidebar.jsx
+++ frontend/war/src/components/Sidebar.jsx
@@ -10,21 +10,28 @@ const Sidebar = () => {
     const nav = useNavigate();
     return (
         <>
-            <nav className="flex left-0 top-0  bg-gray-200 justify-center items-center text-3xl text-gray-800 h-[80px]  cursor-pointer space-x-11">
-                {!user && (
-                    <>
-                        <div onClick={() => nav('/register')} className='transition duration-100 hover:text-yellow-400'><FaRegRegistered /></div>
-                        <div onClick={() => nav('/login')} className='transition duration-100 hover:text-yellow-400'> <IoMdLogIn /></div>
-                    </>
-                )}
+            <nav className="flex left-0 top-0 bg-gray-200 justify-between items-center text-3xl text-gray-800 h-[80px] cursor-pointer px-10">
+                <div className="flex space-x-11">
+                    {!user && (
+                        <>
+                            <div onClick={() => nav('/register')} className='transition duration-100 hover:text-yellow-400'><FaRegRegistered /></div>
+                            <div onClick={() => nav('/login')} className='transition duration-100 hover:text-yellow-400'> <IoMdLogIn /></div>
+                        </>
+                    )}
+                    {user && (
+                        <>
+                            <div onClick={() => nav('/logOut')} className='transition duration-100 hover:text-yellow-400' > <BiLogOutCircle /></div>
+                        </>
+                    )}
+                    <div onClick={() => nav('/contact')} className='transition duration-100 hover:text-yellow-400'> <FaComments /></div>
+                    <div onClick={() => nav('/soldiers')} className='transition duration-100 hover:text-yellow-400' > <FaUserAlt /></div>
+                    <div onClick={() => nav('/homePage')} className='transition duration-100 hover:text-yellow-400'><FaHome /></div>
+                </div>
                 {user && (
-                    <>
-                        <div onClick={() => nav('/logOut')} className='transition duration-100 hover:text-yellow-400' > <BiLogOutCircle /></div>
-                    </>
+                    <div className="text-lg text-gray-800 font-semibold">
+                        שלום, {user.Name}
+                    </div>
                 )}
-                <div onClick={() => nav('/contact')} className='transition duration-100 hover:text-yellow-400'> <FaComments /></div>
-                <div onClick={() => nav('/soldiers')} className='transition duration-100 hover:text-yellow-400' > <FaUserAlt /></div>
-                <div onClick={() => nav('/homePage')} className='transition duration-100 hover:text-yellow-400'><FaHome /></div>
             </nav>
             <div className='flex items-center mb-1'>
                 <img className="mt-3 ml-5 w-[10px] mr-15 " src="/חץ חזור.svg" alt="Logo" onClick={() => nav(-2)} />
