80241e058301b05785d29864ed83c8fa46444ed9
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-20 23:46:40 +0300

    jj

--- frontend/war/src/index.js
+++ frontend/war/src/index.js
@@ -9,6 +9,21 @@ import { Provider } from "react-redux";
 import soliderSlice from './features/soliderSlice';
 import userSlice from './features/userSlice';
 
+// Google Tag Manager
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js'
+  });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != 'dataLayer' ? '&l=' + l : '';
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-NG3MWHDC');
+
 const store = configureStore({
   reducer: {
     user: userSlice,
@@ -18,11 +33,19 @@ const store = configureStore({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <>
+    {/* Google Tag Manager (noscript) */}
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NG3MWHDC"
+        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+    </noscript>
+    {/* End Google Tag Manager (noscript) */}
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
