8fe09207c3db19e8332680b63e331a5b82398b15
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-20 13:47:51 +0300

    jj

--- frontend/war/src/components/Login.jsx
+++ frontend/war/src/components/Login.jsx
@@ -74,8 +74,8 @@ const Login = () => {
                 <div onClick={() => nav('/homePage')} className='transition duration-100 hover:text-yellow-400'><FaHome /></div>
             </nav>
             <div className="flex flex-wrap justify-center mt-4 text-center h-screen text-gray-800">
-                <div className="bg-white text-center mt-4 mr-2 ml-2  ">
-                    <form onSubmit={handleClickLogin} className="space-y-4 p-6 w-full max-w-md shadow-top shadow-gray-800 rounded-2xl hover:animate-button-push hover:shadow-xl hover:shadow-gray-700">
+                <div className="text-center mt-4 mr-2 ml-2">
+                    <form onSubmit={handleClickLogin} className="bg-white space-y-4 p-6 w-full max-w-md shadow-top shadow-gray-800 rounded-2xl hover:animate-button-push hover:shadow-xl hover:shadow-gray-700">
                         <input name="email" type="text" placeholder="מייל" value={email} onChange={handleChangeEmail} style={{ direction: 'rtl' }} className="mb-2 bg-gray-200 rounded-lg p-2 w-full" />
                         <input name="password" type="password" placeholder="סיסמא" value={password} onChange={handleChangePassword} style={{ direction: 'rtl' }} className="mb-2 bg-gray-200 rounded-lg p-2 w-full" />
                         <span className="text-red-500 mb-2">{error}</span>
