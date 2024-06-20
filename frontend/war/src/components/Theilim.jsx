f0ec4026c2041a5cf2abe654fd87dc03724961dc
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-20 15:16:40 +0300

    jj

--- frontend/war/src/components/Theilim.jsx
+++ frontend/war/src/components/Theilim.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router';
 import { useSelector } from 'react-redux';
 import { addTehilim, getByUserCountTehilimForSoliderId, getCountTehilimBySoliderId, getTehilimBySoliderIdUser, updateTehilim } from '../utils/TehilimUtil';
