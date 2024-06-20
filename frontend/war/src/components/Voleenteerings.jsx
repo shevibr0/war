e0b227b2839b57d3320043a615ded268e9ba3f2f
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-20 11:27:46 +0300

    add

--- frontend/war/src/components/Voleenteerings.jsx
+++ frontend/war/src/components/Voleenteerings.jsx
@@ -50,34 +50,26 @@ const Voleenteerings = () => {
             <div className='flex justify-center cursor-pointer' onClick={() => nav(`/soldierInfo/${id}/addVolunteer`)}>
                 + הוסף התנדבות
             </div>
-            <div className=''>
-
-                {isLoading ? (
-                    <p>Loading...</p>
-                ) : (
-                    <div className="mt-3 bg-gray-200">
-                        <ul style={{ direction: 'rtl' }} className="flex flex-wrap justify-center  bg-gray-200">
-                            {volunteeringOptions.map(option => (
-                                <li key={option.Id} className="bg-yellow-100 shadow-l lg:w-1/5 md:w-1/4 sm:w-1/3 w-1/2 p-4 m-2 rounded-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col justify-between">
-                                    <strong className="text-black"> {option.Description} </strong><br />
-                                    <p className='text-gray-400 text-sm'>נכתב בתאריך {new Date(option.Date).toLocaleDateString()}</p>
-                                    <p className=' text-gray-400'>ע"י {option.IdUserNavigation.Name}</p>
-                                    <div className="flex mt-0 pt-2 justify-end">
-                                        <button onClick={() => handleEdit(option.Id)} className="text-black hover:text-red-700">
-                                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21H3v-3.5L14.732 3.732z"></path></svg>
-                                        </button>
-                                        <button onClick={() => handleDelete(option.Id)} className="text-black hover:text-red-700">
-                                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
-                                        </button>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-
+            <div className="mt-3">
+                <ul style={{ direction: 'rtl' }} className="flex flex-wrap justify-center bg-gray-200">
+                    {volunteeringOptions.map(option => (
+                        <li key={option.Id} className="bg-yellow-100 shadow-l lg:w-1/5 md:w-1/4 sm:w-1/3 w-1/2 p-4 m-2 rounded-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col justify-between">
+                            <strong className="text-black"> {option.Description} </strong><br />
+                            <p className='text-gray-400 text-sm'>נכתב בתאריך {new Date(option.Date).toLocaleDateString()}</p>
+                            <p className=' text-gray-400'>ע"י {option.IdUserNavigation.Name}</p>
+                            <div className="flex mt-0 pt-2 justify-end">
+                                <button onClick={() => handleEdit(option.Id)} className="text-black hover:text-red-700">
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21H3v-3.5L14.732 3.732z"></path></svg>
+                                </button>
+                                <button onClick={() => handleDelete(option.Id)} className="text-black hover:text-red-700">
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
-        </div >
+        </div>
     )
 }
 
