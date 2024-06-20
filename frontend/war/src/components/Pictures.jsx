7fb4a381a9d454afc1078c15bd3f9727b922b66d
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-21 02:39:53 +0300

    jj

--- frontend/war/src/components/Pictures.jsx
+++ frontend/war/src/components/Pictures.jsx
@@ -78,8 +78,8 @@ const Pictures = () => {
                         <h3 className='font-bold text-gray-800'>בנימה אישית</h3>
                         {image.PersonalWords && <p className='text-sm italic text-gray-500 mb-2 break-words whitespace-pre-wrap'>{image.PersonalWords}</p>}
                         {user && user.Id === image.IdUser && (
-                            <div className="flex mt-0 pt-2 justify-end">
-                                <button onClick={() => handleDelete(image.Id)} className="text-black hover:text-red-700">
+                            <div className="flex mt-0 pt-2 justify-start">
+                                <button onClick={() => handleDelete(image.Id)} className="text-gray-800 hover:text-red-700">
                                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                                 </button>
                             </div>
