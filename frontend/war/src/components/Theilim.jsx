847539f274051d8f9b98b51a528be6c497896152
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-20 14:42:29 +0300

    jj

--- frontend/war/src/components/Theilim.jsx
+++ frontend/war/src/components/Theilim.jsx
@@ -5,31 +5,28 @@ import { addTehilim, getByUserCountTehilimForSoliderId, getCountTehilimBySolider
 import Sidebar from './Sidebar';
 
 const Theilim = () => {
-    const nav = useNavigate()
+    const nav = useNavigate();
     const [num, setNum] = useState(0);
     const [userNum, setUserNum] = useState(0);
     const [selectedPsalms, setSelectedPsalms] = useState(null);
     const [selectedPsalmsPart, setSelectedPsalmsPart] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
     const { id } = useParams();
-    const [isOpen, setIsOpen] = useState(false);
     const user = useSelector(state => state.user.connectedUser);
     const [theilimUser, setTheilimUser] = useState(null);
-
-
 
     useEffect(() => {
         if (selectedPsalms) {
             console.log("Selected Psalms Updated:", selectedPsalms);
         }
-    }, [selectedPsalms]);  // יופעל כל פעם שselectedPsalms משתנה
+    }, [selectedPsalms]);
+
     const fetchTehilimBySoliderIdUser = async () => {
         try {
             const res = await getTehilimBySoliderIdUser(user.Id, id);
-            if (res.status == 204) {
+            if (res.status === 204) {
                 setTheilimUser(null);
-            }
-            else {
+            } else {
                 setTheilimUser(res.data);
             }
             const dataCount = await getCountTehilimBySoliderId(id);
@@ -39,7 +36,6 @@ const Theilim = () => {
             setUserNum(dataCountUser);
         } catch (error) {
             console.log(error);
-            // alert("אופס ארעה תקלה נסה שוב מאוחר יותר...");
         }
     };
 
@@ -48,7 +44,6 @@ const Theilim = () => {
     }, [user]);
 
     const handleAddTheilimForSolider = async (e) => {
-        debugger;
         e.preventDefault();
         if (theilimUser == null) {
             // add new theilim
@@ -58,27 +53,25 @@ const Theilim = () => {
                 IdUser: user.Id,
                 Count: 1,
                 Date: new Date().toISOString()
-            }
+            };
             await addTehilim(theilimEmpty).then(res => {
                 setTheilimUser(theilimEmpty);
-                setNum(num + 1);
-                setUserNum(userNum + 1);
+                setNum(prevNum => prevNum + 1);
+                setUserNum(prevUserNum => prevUserNum + 1);
                 setShowPopup(false);
             });
-        }
-        else {
+        } else {
             let _theilimUser = { ...theilimUser };
             _theilimUser.Count = _theilimUser.Count + 1;
             console.log(_theilimUser);
             //update theilim
             await updateTehilim(_theilimUser.Id, _theilimUser).then(res => {
                 setTheilimUser(_theilimUser);
-                setNum(num + 1);
+                setNum(prevNum => prevNum + 1);
                 setShowPopup(false);
-            })
-
+            });
         }
-    }
+    };
 
     const numberToHebrewLetter = (num) => {
         const hebrewLetters = [
@@ -97,15 +90,14 @@ const Theilim = () => {
             'קכא', 'קכב', 'קכג', 'קכד', 'קכה', 'קכו', 'קכז', 'קכח', 'קכט', 'קל',
             'קלא', 'קלב', 'קלג', 'קלד', 'קלה', 'קלו', 'קלז', 'קלח', 'קלט', 'קמ',
             'קמא', 'קמב', 'קמג', 'קמד', 'קמה', 'קמו', 'קמז', 'קמח', 'קמט', 'קנ',
-
         ];
-        return hebrewLetters[num - 1]; // מחזירים את האות המתאימה
+        return hebrewLetters[num - 1];
     };
+
     const fetchPsalms = async (part) => {
         const response = await fetch(`https://www.sefaria.org/api/bulktext/Psalms.${part}`);
         if (response.ok) {
             const data = await response.json();
-            // וודא שהנתיב להעתק העברי קיים בנתונים שהתקבלו לפני שתנסה לקרוא ממנו
             if (data && data[`Psalms.${part}`] && data[`Psalms.${part}`].he) {
                 setSelectedPsalms(data[`Psalms.${part}`].he);
                 setSelectedPsalmsPart(numberToHebrewLetter(part));
@@ -116,33 +108,34 @@ const Theilim = () => {
         } else {
             console.error('Failed to fetch data');
         }
-    }
+    };
+
     return (
-        <div className="bg-gray-200">
+        <div className="bg-gray-200 min-h-screen">
             <Sidebar />
-            <div className='flex justify-center'>
-                מספר פרקי תהילים שנאמרו {num} עי כמות משתתפים {userNum}
+            <div className='flex justify-center mt-4'>
+                <p>מספר פרקי תהילים שנאמרו: {num} | כמות משתתפים: {userNum}</p>
             </div>
-            <div className="bg-gray-200 min-h-screen  flex flex-col items-center mt-0" style={{ direction: 'rtl' }}>
+            <div className="bg-gray-200 min-h-screen flex flex-col items-center mt-0" style={{ direction: 'rtl' }}>
                 <h1 className="text-2xl font-semibold mb-5">בחר פרק תהילים</h1>
                 <div className="grid lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-3 gap-2">
                     {Array.from({ length: 150 }, (_, i) => (
                         <button
                             key={i + 1}
-                            className="bg-gray-600 hover:bg-white text-white hover:text-gray-600 hover:border border-gray-600 font-bold py-2 px-7 rounded"
+                            className="bg-gray-600 hover:bg-white text-white shadow-xl shadow-gray-800 hover:text-gray-600 hover:border border-gray-600 font-bold py-2 px-7 rounded mb-2"
                             onClick={() => fetchPsalms(i + 1)}>
                             {numberToHebrewLetter(i + 1)}
                         </button>
                     ))}
                 </div>
                 {showPopup && (
-                    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center overflow-y-auto">
-                        <div className="bg-white p-5 rounded-lg max-w-lg w-full mx-2 my-4 ">
+                    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center overflow-y-auto">
+                        <div className="bg-white p-5 rounded-lg max-w-lg w-full mx-2 my-4">
                             <h2 className="font-bold text-xl mb-2">תהילים פרק {selectedPsalmsPart}</h2>
                             <div className='h-[350px] overflow-y-auto'>
                                 <p className="whitespace-pre-line" dangerouslySetInnerHTML={{ __html: selectedPsalms }} />
                             </div>
-                            <div className='flex justify-center '>
+                            <div className='flex justify-center'>
                                 <button onClick={handleAddTheilimForSolider}
                                     className="mt-4 bg-gray-600 hover:bg-white text-white hover:text-gray-600 hover:border border-gray-600 font-bold py-2 px-4 rounded">
                                     קראתי את הפרק
@@ -160,4 +153,4 @@ const Theilim = () => {
     )
 }
 
-export default Theilim
+export default Theilim;
