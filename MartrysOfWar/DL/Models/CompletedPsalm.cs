c6a9930917da28f3f3b928eac903e2213a632a96
Elisheva Einhoren <shevibr0@gmail.com>
2024-06-25 00:47:36 +0300

    add

--- MartrysOfWar/DL/Models/CompletedPsalm.cs
+++ MartrysOfWar/DL/Models/CompletedPsalm.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DL.Models;
 
-namespace DL.Models
+public partial class CompletedPsalm
 {
-    public partial class CompletedPsalm
-    {
-        public int Id { get; set; }
-        public int IdSoldier { get; set; }
-        public int IdUser { get; set; }
-        public int PsalmNumber { get; set; }
+    public int Id { get; set; }
+    public int IdSoldier { get; set; }
+    public int IdUser { get; set; }
+    public int PsalmNumber { get; set; }
 
-      
-    }
+    public virtual Soldier IdSoldierNavigation { get; set; } = null!;
+    public virtual User IdUserNavigation { get; set; } = null!;
 }
