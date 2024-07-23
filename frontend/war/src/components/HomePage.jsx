671c58487fb61bcf2e1575165f6a3731762642a4
Elisheva Einhoren <shevibr0@gmail.com>
2024-07-23 11:05:45 +0300

    add credit

--- frontend/war/src/components/HomePage.jsx
+++ frontend/war/src/components/HomePage.jsx
@@ -11,7 +11,32 @@ const HomePage = () => {
 
     return (
         <div className="bg-gray-200 h-screen font-sans">
-            <Sidebar />
+            <>
+                <nav className="flex left-0 top-0  bg-gray-200 justify-center items-center text-3xl text-gray-800 h-[80px]  cursor-pointer space-x-11">
+                    {!user && (
+                        <>
+                            <div onClick={() => nav('/login')} className='transition duration-100 hover:text-yellow-400'> <IoMdLogIn /></div>
+                        </>
+                    )}
+                    {user && (
+                        <>
+                            <div onClick={() => nav('/logOut')} className='transition duration-100 hover:text-yellow-400' > <BiLogOutCircle /></div>
+                        </>
+                    )}
+                    <div onClick={() => nav('/contact')} className='transition duration-100 hover:text-yellow-400'> <FaComments /></div>
+                    <div onClick={() => nav('/soldiers')} className='transition duration-100 hover:text-yellow-400' > <FaSearch /></div>
+                    <div onClick={() => nav('/homePage')} className='transition duration-100 hover:text-yellow-400'><FaHome /></div>
+                </nav>
+                <h3 className='flex justify-center mt-2 mb-2 text-red-600 text-xs'>"המידע אודות החיילים מתעדכן מידי יום מהאתר "דבר</h3>
+                <h3 className='flex justify-center text-red-600 text-sm underline'><a href="https://www.davar1.co.il/" target="_blank" rel="noopener noreferrer">קרדיט לאתר דבר </a></h3>
+                <div className='flex justify-center mt-2 mb-2'>
+                    {user && (
+                        <div className="text-lg text-gray-800 font-semibold text-center">
+                            {user.Name}  שלום
+                        </div>
+                    )}
+                </div>
+            </>
             <div className='bg-gray-200'>
                 <div className='flex justify-center'>
                     <h2 className='text-gray-700 text-3xl font-bold'>מנציחים את זכרם</h2><br />
